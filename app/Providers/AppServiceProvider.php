bd6c453d931db66e1398eac9e36c0a19292fe642
iffahradzii <164728400+iffahradzii@users.noreply.github.com>
2025-06-21 00:54:46 +0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production')) {
+            URL::forceRootUrl(config('app.url'));
             URL::forceScheme('https');
         }
     }
